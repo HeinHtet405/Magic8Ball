285821f0d4c770d48176ac74995fc029b56ebbba
heinhtet405 <heinhtet.hh405@gmail.com>
2017-09-14 20:00:13 +0630

    Exercise complete

--- Magic8ball/ViewController.swift
+++ Magic8ball/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let ballArray = ["ball1","ball2","ball3","ball4","ball5"]
+    
+    var randomBallNumber = 0
 
+    @IBOutlet weak var answerBallImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        newBallImage()
+    }
+    
+    override var prefersStatusBarHidden: Bool{
+        return true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pressedAskBtn(_ sender: Any) {
+        newBallImage()
     }
-
+    
+    func newBallImage(){
+        randomBallNumber = Int(arc4random_uniform(5))
+        answerBallImageView.image = UIImage(named: ballArray[randomBallNumber])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
 
 }
 
